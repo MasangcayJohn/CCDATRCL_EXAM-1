d0fd09d6c620f2995c016bc9f337df72d305e030
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-09-12 22:48:12 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,55 @@
+
+import java.util.Stack;
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    // ARRAYS (50 points)
+    // ======================================================
+    // 1. Create an array of characters of your SURNAME.
+
+    // 2. Display each character of your SURNAME on each line.
+
+    // 3. Display the first character of your SURNAME.
+
+    // 4. Display the last character of your SURNAME.
+
+    // 5. Display the character of your SURNAME in reverse order.
+
+    // LINKED LIST (25 points)
+    // ======================================================
+    // 1. Create a Linked list of the last six numbers of your STUDENT NUMBER.
+
+    // 2. Add an asterisk (*) to the head/front of the Linked list.
+
+    // 3. Add an asterisk (*) to the tail/end of the Linked list.
+  
+    // 4. Display the Linked list.
+
+    // 5. Display the last character of the Linked list.
+  
+    // 6. Replace the last character of the Linked list with the exclamation symbol (!).
+
+    // 7. Remove the first character of the Linked list.
+
+    // 8. Display the LinkedList.
+
+    // STACK (25 points)
+    // ======================================================
+    // 1. Create a Stack of characters of your SURNAME.
+
+    // 2. Print the Stack.
+
+    // 3. Add a "INF214" to the stack.
+
+    // 4. Print the Stack.
+
+    // 5. Remove "INF214".
+
+    // 6. Add "CCDATRCL-INF214".
+
+    // 7. Print the Stack.
+
+    }
+}
